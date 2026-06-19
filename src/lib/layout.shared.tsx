0707dc2813edbca1b162f04bc7fdc8a6e0a4ae9b
WarFiN <82953540+WarFiN123@users.noreply.github.com>
2026-06-20 03:16:34 +0530

    more improvements

--- src/lib/layout.shared.tsx
+++ src/lib/layout.shared.tsx
@@ -1,10 +1,23 @@
 import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared";
+import icon from "@/../public/logo.svg";
+import Image from "next/image";
 
 export function baseOptions(): BaseLayoutProps {
   return {
     githubUrl: "https://github.com/Uncover-it/docs",
     nav: {
-      title: "Uncover it - Documentation",
+      title: (
+        <span className="inline-flex text-[0.9375rem] items-center gap-1.5 font-medium me-auto">
+          <Image
+            src={icon}
+            alt="Uncover it"
+            width={22}
+            height={22}
+            className="dark:invert"
+          />
+          Documentation
+        </span>
+      ),
     },
   };
 }
